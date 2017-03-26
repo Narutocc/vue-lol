0b59b060bfc7a9bf45ca74a0b5691fd2721c32eb
liyongchun <1044389299@qq.com>
2017-03-26 17:12:26 +0800

    add

--- components/wchampiondetail.vue
+++ components/wchampiondetail.vue
@@ -1,9 +1,20 @@
 <template>
 	<div>
-		<img :src="'http://cdn.tgp.qq.com/pallas/images/skins/original/'+key+'-'+id+'.jpg'"/>
+		<div class="header">
+		    <span class="arrow" @click="back"><</span>
+		    <span style="margin-left:112px">{{list.name}}</span>
+		 </div>
+		<img :src="'http://cdn.tgp.qq.com/pallas/images/skins/original/'+key+'-'+id+'.jpg'" class="big"/>
+		<div class="infoes">
+			<p>{{list.name}}</p>
+			<p>{{list.id}}</p>
+			<p>属性：</p>
+			<p><span>攻：{{list.info.attack}}</span><span>法：{{list.info.magic}}</span></p>
+			<p><span>防：{{list.info.defense}}</span><span>操作：{{list.info.difficulty}}</span></p>
+		</div>
 		<div class="container">
-			<span @click="show(1)" :class="{'active':page1}">技能</span>
-			<span @click="show(2)" :class="{'active':page2}">皮肤</span>
+			<span @click="show(1)" :class="{'active':page1}">技能说明</span>
+			<span @click="show(2)" :class="{'active':page2}">皮肤欣赏</span>
 		</div>
 		<div class="spell" v-if="page1">
 			<div v-for="spell in spells" class="detail">
@@ -17,11 +28,14 @@
 			</div>
 		</div>
 		<div class="skin" v-if="page2">
+			<p>点击皮肤可以观看介绍视频哦</p>
 			<div v-for="skin in skins">
-				<a :href="skin.displayUrl">
-					<img :src="'http://cdn.tgp.qq.com/pallas/images/skins/original/'+key+'-'+skin.id+'.jpg'" />
-				</a>
-				<p>{{skin.name}}</p>
+				<div style="text-align:center">
+					<a :href="skin.displayUrl">
+						<img :src="'http://cdn.tgp.qq.com/pallas/images/skins/original/'+key+'-'+skin.id+'.jpg'" />
+					</a>
+					<p class="name">{{skin.name}}</p>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -31,35 +45,68 @@
 		margin:0;
 		padding:0;
 	}
-	img{
+	.big{
+		margin-top:56px;
 		width:100%;
-		height:240px;
+		height:200px;
 	}
 	.container{
-		height:56px;
-		line-height:56px;
-		background-color:#ccc;
+		height:52px;
+		line-height:52px;
+		background-color:#6058aa;
+		margin-top:-6px;
 	}
-	.container a{
-		color:#000;
-		font-size:18px;
-		margin:40px;
+	.container span{
+		color:#fff;
+		font-size:16px;
+		display:inline-block;
+		width:49%;
+		text-align:center;
 	}
 	.detail{
 		overflow:hidden;
+		margin:15px 10px;
 	}
 	.detail img{
-		width:50px;
-		height:50px;
+		width:100px;
+		height:100px;
 	}
 	.left{
 		float:left;
 	}
 	.right{
 		float:right;
+		width:250px;
 	}
-	.active{
-		color:red;
+	.right p:first-child{
+		font-weight:bold;
+	}
+	.container span.active{
+		color:#000;
+		font-weight:bold;
+		border-bottom:1px solid red;
+	}
+	.skin p{
+		margin:10px;
+		font-size:16px;
+		border-bottom:2px dotted #ccc;
+	}
+	.skin img{
+		width:80%;
+		height:100%;
+	}
+	.skin .name{
+		font-size:16px;
+		margin-bottom:20px;
+		letter-spacing:2px;
+		font-weight:bold;
+	}
+	.infoes{
+		position:absolute;
+		left:28px;
+		top:95px;
+		color:#fff;
+		line-height:22px;
 	}
 </style>
 <script>
@@ -104,6 +151,9 @@
 					this.page1 = false;
 					this.page2 = true;
 				}
+			},
+			back:function(){
+				window.history.go(-1);
 			}
 		},
 		mounted:function(){
